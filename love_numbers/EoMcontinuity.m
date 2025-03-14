44eb6680a9f5d29028c0fb2d779002c18ac81405
masellia <88507034+masellia@users.noreply.github.com>
2025-03-14 14:49:10 +0100

    Add files via upload

--- love_numbers/EoMcontinuity.m
+++ love_numbers/EoMcontinuity.m
@@ -1,11 +1,5 @@
 (* Created with the Wolfram Language : www.wolfram.com *)
-{(4*Pi*\[Epsilon]*Y[\[Theta], \[Phi]]*
-   (r*B[r]*H1[r]*(p[r] + \[Rho][r])*Derivative[1][A][r] + 
-    A[r]*(r*H1[r]*(p[r] + \[Rho][r])*Derivative[1][B][r] + 
-      2*B[r]*(r*(p[r] + \[Rho][r])*Derivative[1][H1][r] + 
-        H1[r]*(2*p[r] + 2*\[Rho][r] + r*(Derivative[1][p][r] + 
-            Derivative[1][\[Rho]][r]))))))/(r*A[r]), 
- (4*Pi*(p[r] + \[Rho][r])*Derivative[1][A][r])/A[r] + 
+{0, (4*Pi*(p[r] + \[Rho][r])*Derivative[1][A][r])/A[r] + 
   8*Pi*Derivative[1][p][r] + (4*Pi*\[Epsilon]*Y[\[Theta], \[Phi]]*
     ((1 + dpd\[Rho])*\[Delta]p[r]*Derivative[1][A][r] + 
      dpd\[Rho]*A[r]*(-((p[r] + \[Rho][r])*Derivative[1][H0][r]) + 
--- love_numbers/stressenergy.m
+++ love_numbers/stressenergy.m
@@ -1,8 +1,7 @@
 (* Created with the Wolfram Language : www.wolfram.com *)
 {{(-8*Pi*\[Epsilon]*Y[\[Theta], \[Phi]]*\[Delta]p[r])/dpd\[Rho] - 
-   8*Pi*\[Rho][r], 0, 0, 0}, 
- {\[Epsilon]*(8*Pi*B[r]*H1[r]*p[r]*Y[\[Theta], \[Phi]] + 
-    8*Pi*B[r]*H1[r]*Y[\[Theta], \[Phi]]*\[Rho][r]), 
-  8*Pi*p[r] + 8*Pi*\[Epsilon]*Y[\[Theta], \[Phi]]*\[Delta]p[r], 0, 0}, 
+   8*Pi*\[Rho][r], (8*\[Epsilon]*(Pi*H1[r]*p[r]*Y[\[Theta], \[Phi]] + 
+     Pi*H1[r]*Y[\[Theta], \[Phi]]*\[Rho][r]))/A[r], 0, 0}, 
+ {0, 8*Pi*p[r] + 8*Pi*\[Epsilon]*Y[\[Theta], \[Phi]]*\[Delta]p[r], 0, 0}, 
  {0, 0, 8*Pi*p[r] + 8*Pi*\[Epsilon]*Y[\[Theta], \[Phi]]*\[Delta]p[r], 0}, 
  {0, 0, 0, 8*Pi*p[r] + 8*Pi*\[Epsilon]*Y[\[Theta], \[Phi]]*\[Delta]p[r]}}
